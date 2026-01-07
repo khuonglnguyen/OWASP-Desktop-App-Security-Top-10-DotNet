de2e62f7f55d18cf3d6908361f0fd66e158696ff
Nguyen Lap An Khuong / クウン <khuong.nguyen@deltabrains.vn>
2026-01-07 16:26:18 +0700

    Add JWT example with hardcoded secret; update configs

--- OWASP_Desktop_App_Security_Top_10/Program.cs
+++ OWASP_Desktop_App_Security_Top_10/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace OWASP_Desktop_App_Security_Top_10
+﻿using JWT.Algorithms;
+using JWT.Builder;
+
+namespace OWASP_Desktop_App_Security_Top_10
 {
     static class Program
     {
         static void Main()
         {
+            var secret = "secret123";
+            var builder = JwtBuilder.Create();
+            builder.WithAlgorithm(new HMACSHA256Algorithm());
+            // ruleid: jwt-hardcoded-secret
+            builder.WithSecret(secret);
+            builder.AddClaim("exp", DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds());
+            builder.AddClaim("claim1", 0);
+            builder.AddClaim("claim2", "claim2-value");
 
+            var token = builder.Encode();
+
+            Console.WriteLine(token);
         }
     }
 }
