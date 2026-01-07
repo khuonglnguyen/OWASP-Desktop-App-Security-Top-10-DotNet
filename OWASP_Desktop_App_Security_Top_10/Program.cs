952d79a7887c94674131000921bb3ace0d5998b3
Nguyen Lap An Khuong / クウン <khuong.nguyen@deltabrains.vn>
2026-01-07 16:38:29 +0700

    Add MD5 usage and update SonarQube config files

--- OWASP_Desktop_App_Security_Top_10/Program.cs
+++ OWASP_Desktop_App_Security_Top_10/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace OWASP_Desktop_App_Security_Top_10
+﻿using System.Security.Cryptography;
+
+namespace OWASP_Desktop_App_Security_Top_10
 {
     static class Program
     {
         static void Main()
         {
+            MD5 md5 = MD5.Create();
         }
     }
 }
