8f3ed7ae85d05355d8ed9a793590de2c1725a7ce
Nguyen Lap An Khuong / クウン <khuong.nguyen@deltabrains.vn>
2026-01-08 11:47:01 +0700

    Add SonarQube config files and sensitive file access demo

--- OWASP_Desktop_App_Security_Top_10/Program.cs
+++ OWASP_Desktop_App_Security_Top_10/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace OWASP_Desktop_App_Security_Top_10
+﻿using System.Security.AccessControl;
+
+namespace OWASP_Desktop_App_Security_Top_10
 {
     static class Program
     {
         static void Main()
         {
-            
+            var accessRule = new FileSystemAccessRule("Everyone", FileSystemRights.Write, AccessControlType.Allow);
+            var fileInfo = new FileInfo("path");
+            var fileSecurity = fileInfo.GetAccessControl();
+
+            fileSecurity.SetAccessRule(accessRule); // Sensitive
+            fileInfo.SetAccessControl(fileSecurity);
         }        
     }
 }
