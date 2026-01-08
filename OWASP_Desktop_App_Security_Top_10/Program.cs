5dacc86d156038af74d1145e68d414b2ed47188a
Nguyen Lap An Khuong / クウン <khuong.nguyen@deltabrains.vn>
2026-01-08 16:33:33 +0700

    Add SonarQube config and logging anti-pattern examples

--- OWASP_Desktop_App_Security_Top_10/Program.cs
+++ OWASP_Desktop_App_Security_Top_10/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace OWASP_Desktop_App_Security_Top_10
+﻿using Microsoft.Extensions.Logging;
+
+namespace OWASP_Desktop_App_Security_Top_10
 {
     static class Program
     {
+        private static readonly ILogger logger = LoggerFactory.Create(builder => { }).CreateLogger(nameof(Program));
+
         static void Main()
         {
-            
-        }        
+            string user = "alice";
+            logger.LogError("Login failed for {User", user);       // Noncompliant: Syntactically incorrect
+            logger.LogError("Login failed for {}", user);          // Noncompliant: Empty placeholder
+            logger.LogError("Login failed for {User-Name}", user); // Noncompliant: Only letters, numbers, and underscore are allowed for placeholders
+        }
     }
 }
