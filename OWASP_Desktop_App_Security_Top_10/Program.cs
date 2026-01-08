0537f237f8c24a7268a1b81fe4211de6121583fe
Nguyen Lap An Khuong / クウン <khuong.nguyen@deltabrains.vn>
2026-01-08 09:23:16 +0700

    Add SonarQube config files and demo weak hash usage

--- OWASP_Desktop_App_Security_Top_10/Program.cs
+++ OWASP_Desktop_App_Security_Top_10/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace OWASP_Desktop_App_Security_Top_10
+﻿using System.Security.Cryptography;
+
+namespace OWASP_Desktop_App_Security_Top_10
 {
     static class Program
     {
         static void Main()
         {
-            
+            var hashProvider4 = HashAlgorithm.Create("MD5");
         }        
     }
 }
