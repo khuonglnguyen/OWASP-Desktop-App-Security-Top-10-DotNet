d874d29b382f23ca2cfa507c2463a2cd16466df1
Nguyen Lap An Khuong / クウン <khuong.nguyen@deltabrains.vn>
2026-01-08 14:01:02 +0700

    Integrate SonarQube configs and add unsafe SubarraySum

--- OWASP_Desktop_App_Security_Top_10/Program.cs
+++ OWASP_Desktop_App_Security_Top_10/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main()
         {
-            
-        }        
+
+        }
+
+        public static unsafe int SubarraySum(int[] array, int start, int end)  // Sensitive
+        {
+            var sum = 0;
+
+            // Skip array bound checks for extra performance
+            fixed (int* firstNumber = array)
+            {
+                for (int i = start; i < end; i++)
+                    sum += *(firstNumber + i);
+            }
+
+            return sum;
+        }
     }
 }
