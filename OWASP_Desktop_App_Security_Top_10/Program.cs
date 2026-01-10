9af63f5a64bdece02e9359ea32484d390094746f
Nguyen Lap An Khuong / クウン <khuong.nguyen@deltabrains.vn>
2026-01-10 10:43:05 +0700

    Use MD5CryptoServiceProvider for hashing in Main

--- OWASP_Desktop_App_Security_Top_10/Program.cs
+++ OWASP_Desktop_App_Security_Top_10/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace OWASP_Desktop_App_Security_Top_10
+﻿using System.Security.Cryptography;
+
+namespace OWASP_Desktop_App_Security_Top_10
 {
     static class Program
     {
         static void Main()
         {
-            
-        }        
+            var md5 = new MD5CryptoServiceProvider();
+            // ruleid: insecure-crypto-hash
+            var hashValue = md5.ComputeHash(new byte[] { });
+        }
     }
 }
