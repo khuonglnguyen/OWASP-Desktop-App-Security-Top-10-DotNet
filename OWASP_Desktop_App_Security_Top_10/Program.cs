a229d76182635634913fd3a4753d53dd578a10ba
Nguyen Lap An Khuong / クウン <khuong.nguyen@deltabrains.vn>
2026-01-10 13:12:40 +0700

    Add Publisher/Subscriber event demo to Program.cs

--- OWASP_Desktop_App_Security_Top_10/Program.cs
+++ OWASP_Desktop_App_Security_Top_10/Program.cs
@@ -1,10 +1,34 @@
 ﻿namespace OWASP_Desktop_App_Security_Top_10
 {
+    public class Publisher
+    {
+        public event EventHandler RaiseEvent;
+        public void DoSomething()
+        {
+            RaiseEvent?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class Subscriber
+    {
+        public Subscriber(Publisher pub)
+        {
+            pub.RaiseEvent += Handler;
+        }
+
+        private void Handler(object sender, EventArgs e)
+        {
+            Console.WriteLine("Handled event.");
+        }
+    }
     static class Program
     {
         static void Main()
         {
-            
-        }        
+            Publisher pub = new Publisher();
+            Subscriber sub = new Subscriber(pub);
+
+            pub.DoSomething();
+        }
     }
 }
