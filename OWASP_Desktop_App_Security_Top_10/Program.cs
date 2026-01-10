fda74dcd96bd0f61513c04f90db16e688bbefc31
Nguyen Lap An Khuong / クウン <khuong.nguyen@deltabrains.vn>
2026-01-10 10:59:15 +0700

    Add Foo class with safe and unsafe XML loading examples

--- OWASP_Desktop_App_Security_Top_10/Program.cs
+++ OWASP_Desktop_App_Security_Top_10/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace OWASP_Desktop_App_Security_Top_10
+﻿using System.Xml;
+
+namespace OWASP_Desktop_App_Security_Top_10
 {
     static class Program
     {
         static void Main()
         {
-            
-        }        
+
+        }
+    }
+
+    public class Foo
+    {
+        public void LoadBad(string input)
+        {
+            string fileName = @"C:\Users\user\Documents\test.xml";
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.XmlResolver = new XmlUrlResolver();
+            // ruleid: xmldocument-unsafe-parser-override
+            xmlDoc.Load(input);
+            Console.WriteLine(xmlDoc.InnerText);
+
+            Console.ReadLine();
+        }
+
+        public static void StaticLoadBad(string input)
+        {
+            string fileName = @"C:\Users\user\Documents\test.xml";
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.XmlResolver = new XmlUrlResolver();
+            // ruleid: xmldocument-unsafe-parser-override
+            xmlDoc.Load(input);
+            Console.WriteLine(xmlDoc.InnerText);
+
+            Console.ReadLine();
+        }
+
+        public void LoadGood(string input)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            // ok: xmldocument-unsafe-parser-override
+            xmlDoc.Load(input);
+            Console.WriteLine(xmlDoc.InnerText);
+
+            Console.ReadLine();
+        }
     }
 }
