8ad1cbaf1e519c85c6b8746675448e4fae41de7d
Nguyen Lap An Khuong / クウン <khuong.nguyen@deltabrains.vn>
2026-01-10 13:07:36 +0700

    Set default security protocol to TLS in Main method

--- OWASP_Desktop_App_Security_Top_10/Program.cs
+++ OWASP_Desktop_App_Security_Top_10/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace OWASP_Desktop_App_Security_Top_10
+﻿using System.Net;
+
+namespace OWASP_Desktop_App_Security_Top_10
 {
     static class Program
     {
         static void Main()
         {
-            
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
         }        
     }
 }
