e74e3cec1190e0247ac010fd74390bc18532c465
Nguyen Lap An Khuong / クウン <khuong.nguyen@deltabrains.vn>
2026-01-10 09:57:34 +0700

    Add AES CBC encryption example to Program class

--- OWASP_Desktop_App_Security_Top_10/Program.cs
+++ OWASP_Desktop_App_Security_Top_10/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace OWASP_Desktop_App_Security_Top_10
+﻿using System.Security.Cryptography;
+
+namespace OWASP_Desktop_App_Security_Top_10
 {
     static class Program
     {
         static void Main()
         {
-            
-        }        
+
+        }
+        public static void EncryptWithAesCbc()
+        {
+            Aes key = Aes.Create();
+            // ruleid: cbc-mode
+            key.Mode = CipherMode.CBC;
+            using var encryptor = key.CreateEncryptor();
+            byte[] msg = new byte[32];
+            var cipherText = encryptor.TransformFinalBlock(msg, 0, msg.Length);
+        }
     }
 }
